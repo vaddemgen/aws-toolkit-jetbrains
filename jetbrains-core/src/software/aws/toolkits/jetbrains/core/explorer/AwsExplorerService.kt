f024d0511bc2c43c74b90af90ddcf2f11cebdaa7
Hunter Werlla <werlla@amazon.com>
2020-02-27 15:25:19 -0800

    Initial nodes created

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
@@ -5,6 +5,7 @@ package software.aws.toolkits.jetbrains.core.explorer
 
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
+import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient
 import software.amazon.awssdk.services.ecs.EcsClient
 import software.amazon.awssdk.services.lambda.LambdaClient
@@ -14,6 +15,7 @@ import software.aws.toolkits.jetbrains.core.AwsResourceCache
 import software.aws.toolkits.jetbrains.core.Resource
 import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerServiceRootNode
 import software.aws.toolkits.jetbrains.services.cloudformation.CloudFormationServiceNode
+import software.aws.toolkits.jetbrains.services.cloudwatch.logs.CloudWatchLogsServiceNode
 import software.aws.toolkits.jetbrains.services.ecs.EcsParentNode
 import software.aws.toolkits.jetbrains.services.lambda.LambdaServiceNode
 import software.aws.toolkits.jetbrains.services.s3.S3ServiceNode
@@ -23,6 +25,9 @@ import software.aws.toolkits.resources.message
 enum class AwsExplorerService(val serviceId: String, val displayName: String) {
     CLOUDFORMATION(CloudFormationClient.SERVICE_NAME, message("explorer.node.cloudformation")) {
         override fun buildServiceRootNode(project: Project) = CloudFormationServiceNode(project)
+    },
+    CLOUDWATCH(CloudWatchLogsClient.SERVICE_NAME, message("explorer.node.cloudwatch")) {
+        override fun buildServiceRootNode(project: Project) = CloudWatchLogsServiceNode(project)
     },
     LAMBDA(LambdaClient.SERVICE_NAME, message("explorer.node.lambda")) {
         override fun buildServiceRootNode(project: Project) = LambdaServiceNode(project)
---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/CloudWatchLogsServiceNode.kt
@@ -0,0 +1,39 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.cloudwatch.logs
+
+import com.intellij.openapi.project.Project
+import icons.AwsIcons
+import software.amazon.awssdk.services.cloudformation.CloudFormationClient
+import software.amazon.awssdk.services.cloudwatchlogs.model.LogGroup
+import software.aws.toolkits.jetbrains.core.explorer.AwsExplorerService
+import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerNode
+import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerResourceNode
+import software.aws.toolkits.jetbrains.core.explorer.nodes.CacheBackedAwsExplorerServiceRootNode
+import software.aws.toolkits.jetbrains.services.cloudwatch.logs.resources.CloudWatchResources
+
+class CloudWatchLogsServiceNode(project: Project) : CacheBackedAwsExplorerServiceRootNode<LogGroup>(
+    project,
+    AwsExplorerService.CLOUDWATCH,
+    CloudWatchResources.LIST_LOG_GROUPS
+) {
+    override fun toNode(child: LogGroup): AwsExplorerNode<*> = CloudWatchLogsNode(nodeProject, child.arn(), child.logGroupName())
+}
+
+class CloudWatchLogsNode(
+    project: Project,
+    val arn: String,
+    val logGroupName: String
+) : AwsExplorerResourceNode<String>(
+    project,
+    CloudFormationClient.SERVICE_NAME,
+    logGroupName,
+    AwsIcons.Resources.CLOUDFORMATION_STACK
+) {
+    override fun resourceType() = "stream"
+
+    override fun resourceArn() = arn
+
+    override fun displayName() = logGroupName
+}
